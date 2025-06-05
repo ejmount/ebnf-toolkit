4e01569551241b4aeae039b4a6841370867be8c8
Ewan Mount <ejmount@users.noreply.github.com>
2025-06-06 00:16:59 +0100

    Adding lots of warning flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 #![allow(dead_code)]
+//#![allow(warnings)]
+#![warn(explicit_outlives_requirements)]
+#![warn(missing_debug_implementations)]
+#![forbid(unsafe_code)]
+#![warn(clippy::pedantic)]
+#![warn(missing_copy_implementations)]
+#![warn(redundant_lifetimes)]
+//#![warn(missing_docs)]
+#![warn(unreachable_pub)]
+#![warn(unused_crate_dependencies)]
+#![warn(unused_qualifications)]
 
 use winnow::LocatingSlice;
 
