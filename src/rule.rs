d953241c6ff8888c05344a256a40bfe2e0049c87
Ewan Mount <ejmount@users.noreply.github.com>
2025-08-06 00:22:24 +0100

    Merging grammar rules of the same name

--- src/rule.rs
+++ src/rule.rs
@@ -70,16 +70,49 @@ impl Grammar<'_> {
 
 impl<'a> FromIterator<Rule<'a>> for Grammar<'a> {
     fn from_iter<T: IntoIterator<Item = Rule<'a>>>(iter: T) -> Self {
-        let name_values = iter.into_iter().map(|r| (r.name, r));
-        Grammar {
-            rules: name_values.collect(),
+        let mut rules: HashMap<&'a str, Rule<'a>> = HashMap::new();
+        for (name, new_rule) in iter.into_iter().map(|r| (r.name, r)) {
+            if let hash_map::Entry::Occupied(o) = rules.entry(name) {
+                let old_rule = o.remove();
+                rules.insert(name, merge_duplicate_rule(old_rule, new_rule));
+            } else {
+                rules.insert(name, new_rule);
+            }
         }
+
+        Grammar { rules }
+    }
+}
+
+fn merge_duplicate_rule<'a>(old_rule: Rule<'a>, new_rule: Rule<'a>) -> Rule<'a> {
+    fn unwrap_choice_items(mut e: Vec<Expr<'_>>) -> Vec<Expr<'_>> {
+        if e.len() == 1
+            && let Some(Expr::Choice { .. }) = e.first()
+        {
+            let Some(Expr::Choice { body, .. }) = e.pop() else {
+                unreachable!()
+            };
+            body
+        } else {
+            e
+        }
+    }
+
+    let old_body = unwrap_choice_items(old_rule.body);
+    let new_body = unwrap_choice_items(new_rule.body);
+    let body: Vec<_> = old_body.into_iter().chain(new_body).collect();
+    let span = Span::union(body.iter());
+
+    Rule {
+        name: old_rule.name,
+        body: vec![Expr::Choice { span, body }],
     }
 }
 
 #[cfg(test)]
 mod test {
     use crate::{Expr, Grammar, Rule, token_data::DUMMY_SPAN};
+    use display_tree::AsTree;
 
     #[test]
     fn nonterminals() {
@@ -107,10 +140,52 @@ mod test {
     }
 
     #[test]
-    fn dangling_refs() {
-        let src = "A = B;";
+    fn duplicate_names() {
+        let src = "A = B; A = C; B = A|B; B = C; C = A; C = B|C;  D = C|D; D = A|B;";
         let g = Grammar::new(src).unwrap();
-        let first_dangling = g.first_dangling_reference();
-        insta::assert_compact_debug_snapshot!(first_dangling, @r#"Some(("A", "B"))"#);
+        insta::assert_snapshot!(AsTree::new(&g.rules["A"]), @r"
+        Rule
+        ├─name: A
+        └─0: Choice [1:4..1:12]
+             └─0: Nonterminal [1:4..1:5]
+               │  └─ B
+               1: Nonterminal [1:11..1:12]
+                  └─ C
+        ");
+        insta::assert_snapshot!(AsTree::new(&g.rules["B"]), @r"
+        Rule
+        ├─name: B
+        └─0: Choice [1:18..1:28]
+             └─0: Nonterminal [1:18..1:19]
+               │  └─ A
+               1: Nonterminal [1:20..1:21]
+               │  └─ B
+               2: Nonterminal [1:27..1:28]
+                  └─ C
+        ");
+        insta::assert_snapshot!(AsTree::new(&g.rules["C"]), @r"
+        Rule
+        ├─name: C
+        └─0: Choice [1:34..1:44]
+             └─0: Nonterminal [1:34..1:35]
+               │  └─ A
+               1: Nonterminal [1:41..1:42]
+               │  └─ B
+               2: Nonterminal [1:43..1:44]
+                  └─ C
+        ");
+        insta::assert_snapshot!(AsTree::new(&g.rules["D"]), @r"
+        Rule
+        ├─name: D
+        └─0: Choice [1:51..1:63]
+             └─0: Nonterminal [1:51..1:52]
+               │  └─ C
+               1: Nonterminal [1:53..1:54]
+               │  └─ D
+               2: Nonterminal [1:60..1:61]
+               │  └─ A
+               3: Nonterminal [1:62..1:63]
+                  └─ B
+        ");
     }
 }
